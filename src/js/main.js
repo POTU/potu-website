293471fabce0bee9bdae15e852b9d2f38f12c672
Otso Rasimus <orasimus@gmail.com>
2015-01-31 16:02:35 +0200

    Add a hello world react component.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
+var index = require('./index.jsx');
+
 (function(window) {
     var root = window.document.createElement("div");
     root.id = "root";
     window.document.body.appendChild(root);
-
+    index(root);
 })(window);
