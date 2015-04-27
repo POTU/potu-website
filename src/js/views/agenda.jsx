37bed9f70f75bba0d0f5c334a4863c96c26c9593
Otso Rasimus <orasimus@gmail.com>
2015-04-27 15:46:02 +0300

    Add a link to Github.

--- src/js/views/agenda.jsx
+++ src/js/views/agenda.jsx
@@ -5,6 +5,7 @@ var Agenda = React.createClass({
         return (<div>
             <h2>Agenda</h2>
             <p>The idea of POTU is to get local game teams together once every 2 - 3 months, have an awesome game jam and produce a releasable game. Its all about learning from each other, growing together and having fun!</p>
+            <p>All of the games we have developed are available open source. Check out our <a href="https://github.com/POTU" className="dl-link">Github</a>.</p>
         </div>);
     }
 });
