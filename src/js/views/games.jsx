74396f271b4456db52e1fd6e24872b0d42d242d3
Otso Rasimus <orasimus@gmail.com>
2015-04-27 15:26:14 +0300

    Add Charcabet to the site with download links.

--- src/js/views/games.jsx
+++ src/js/views/games.jsx
@@ -7,6 +7,10 @@ var Games = React.createClass({
             <h3>King Leaf</h3>
             <p>The first game made by POTU is King Leaf. Sven, the king of the leaves, had his throne stolen by the evil Stick of the Sticks. Sven was ripped from his tree and thrown into a river, where he must now try to survive in order to TAKE BACK HIS THRONE!</p>
             <p>King Leaf was made with cocos2d-x.</p>
+            <h3>Charcabet</h3>
+            <p>Eat letters become ztronger!</p>
+            <p>Charcabet was made with Unity. Download here:</p>
+            <p><a href="bin/CharcabetWindows.zip" className="dl-link">Windows</a> | <a href="bin/CharcabetMac.zip" className="dl-link">Macintosh</a> | <a href="bin/Charcabet.apk" className="dl-link">Android (.apk)</a></p>
         </div>);
     }
 });
